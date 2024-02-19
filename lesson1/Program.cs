fa1c098d527ce14235b17dbb147293039271c49d
shavkat <shavkatabdunasimov@gmail.com>
2024-02-19 17:04:00 +0500

    add geekc#:lesson1

--- lesson1/Program.cs
+++ lesson1/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("first code in c#");
+﻿// Console.WriteLine("first code in c#");
+
+Console.WriteLine("write first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("write second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 == num2 * num2)
+{
+    Console.WriteLine("yes");
+}
+else Console.WriteLine("no");
